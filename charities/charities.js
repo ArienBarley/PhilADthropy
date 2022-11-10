d1e0d6150da1934ecc772d8406e6a6db9f318a9e
ArienBarley <arienbarley@gmail.com>
2022-11-09 17:33:37 -0800

    experiments in file reading

--- charities/charities.js
+++ charities/charities.js
@@ -4,6 +4,53 @@ Javascript to run the charities pages of Philadthropy
 Authors: Arien Barley,
 */
 
-//ad and add function
+//variable creation
 
-//
+//no of ads the user has watched this session
+let sessionWatches = 0;
+
+
+//end variabe creation
+
+//functions
+
+//ad an add function
+
+//watch counter
+
+//increment watch count
+
+//when an add has been watched an event listener triggers this function
+function incrementWatches(){
+    // funciton which is triggered each time the user completes an ad watch or add click
+    console.log('function runs');
+    //updates frame of progress bar
+
+    //+1 to add count for this charity in the file
+
+    //update session count on page
+    sessionWatches++;
+    $('#session-count').html(sessionWatches);
+
+    //loads up a new add (possibly refreshes the i-frame)
+};
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("testwrite").innerHTML = this.responseText;
+    }
+  xhttp.open("GET", "data.txt", true);
+  xhttp.send();
+}
+
+
+function testTheData(){
+
+
+}
+//end functions
+
+//button management
+$('#session-count').html(sessionWatches);
+$('#next-ad').click(incrementWatches);
+$('#test').click(loadDoc);
