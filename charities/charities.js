2d3e1c9460fc5916aae7b6a786cf49dbc734af0a
Iyassu Allen <iyassu.allen@gmail.com>
2022-12-01 18:36:06 -0800

    no bg clor ads

--- charities/charities.js
+++ charities/charities.js
@@ -9,116 +9,87 @@ Authors: Arien Barley,
 //no of ads the user has watched this session
 let sessionWatches = 0;
 
-let progressBarFrames = []
 
-const numberOfProgressBarFrames = 5;
-
-const videoIDs = ['HQxwMfqeurQ',
-                  '808ryfU18CQ',
-                  'odI7pQFyjso',
-                  'AvXBN5A5Txc',
-                   'HP6rYThJWUg'];
-
-let noVideos = 1;
 //end variabe creation
 
 //functions
 
-//increment watch count
-function loadNextAd(){
-    //refreshes Iframe(s) with the next video from the list of video IDs
-    console.log('lna running')
-    //generate url with next videoID the modulus causes the list to loop
-    //rather than go off the end and cause an error
-    url = 'https://www.youtube.com/embed/'+videoIDs[sessionWatches%videoIDs.length];
+//ad an add function
 
-    //loop through the iframes
-    for (var i = 1; i<=noVideos;i++){
-        //update their URLs
-        console.log(i);
-        $('#vid'+i).attr('src',url);
-    };
-};
+//increment watch count
 
 //when an add has been watched an event listener triggers this function
 function incrementWatches(){
     // funciton which is triggered each time the user completes an ad watch or add click
+
+    //updates frame of progress bar
 
     //+1 to add count for this charity in the file
     //update session count on page
     sessionWatches++;
     $('#session-count').html(sessionWatches);
 
-    //updates frame of progress bar
-    //if there are more frames avialiable
-    if (sessionWatches < numberOfProgressBarFrames-1){
-        //delete last frame
-        $('#progress-bar img').remove();
-        //epend next frame
-        $('#progress-bar').append(progressBarFrames[sessionWatches]);
-    }//otherwise leave the last frame in place
-
     //loads up a new add (possibly refreshes the i-frame)
-    loadNextAd();
 };
-
-//ads another video to the watching div
-function addAnAd(){
-    //keep track of the number of videos on the page
-    noVideos++;
-    console.log(noVideos);
-    //get the first video and clone it
-    vidFrame = $("#frame1");
-    newFrame = vidFrame.clone(false);
-    //change the id so that we can pull them out s
-    //eperately in the loadNextAd function
-    newFrame.attr('id','frame'+ noVideos);
-    newFrame.find('p iframe').attr('id','vid' + noVideos);
-    //connect the new videoframe into the DOM
-    $("#ad-watch-wrapper").append(newFrame);
-};
-
-$("#ad-an-add").click(function(){
-  addAnAd();
-});
-
-//load progress par frames
-function loadProgressBarFrames(){
-    for (var i = 0; i<numberOfProgressBarFrames -1 ;i++){
-
-        //create element ghost
-        frame = document.createElement('img');
-        frame.src = '../img/progressbarframes/frame'+i+'.jpg';
-
-        //add it to the list
-        progressBarFrames.push(frame);
-    };
-};
-
 //function updateProgressBar()
 
-function loadDoc(path, divID) {
-    //gets the data as text from the passed path (relative to this file)
-    //and writes it to the div with id = divID
+function loadDoc() {
+    console.log('loadDoc runs');
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        $(divID).html = this.responseText;
-    };
-    xhttp.open("GET", path, true);
+        document.getElementById("testwrite").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "data.txt", true);
     xhttp.send();
 };
 
 
 //end functions
-
-//load frames of the progress bar animation
-window.onload = (event) => {
-    loadProgressBarFrames();
-};
-
 //button management
 $('#session-count').html(sessionWatches);
 $('#next-ad').click(incrementWatches);
-$('#test').click(function (){
-    loadDoc("data.txt", "#testwrite");
-});
+$('#test').click(loadDoc);
+//$('#test-play-video').click(playvideo);
+
+
+//gif management
+
+
+/*dump
+
+
+function playvideo(){
+    f = $('#vid1');
+    console.log('clickin iframe content?');
+    console.log(f.contents());
+}
+
+
+function getIframeContent(iframeEl){
+    console.log(iframeEl);
+    contents = iframeEl.contentWindow ?
+            //if true
+            iframeEl.contentWindow.document
+            //if false
+            : iframeEl.contentDocument;
+    console.log(contents);
+    return contents;
+};
+
+
+
+
+if ($('#vid1').contentWindow){
+    console.log('contentWindow exists');
+    $('#vid1').contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+}else if ($('#vid1').contentDocument){
+    console.log('contentDocument exists');
+    $('#vid1').contentDocument.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+}else{
+    console.log('neither exists')
+}
+
+
+console.log(document.getElementById("vid1").contentWindow)
+document.getElementById("vid1").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+*/
